9590da079d056afc8713277be9d22a0783b85310
darshanabk <74397181+darshanabk@users.noreply.github.com>
2024-12-05 13:58:45 +0530

    Update BronzeLayer.py
    Updated to Read dataset through Kaggle API key

--- BronzeLayer.py
+++ BronzeLayer.py
@@ -1,10 +1,15 @@
+import os
 import pandas as pd
 
+# Download the dataset using Kaggle API
+os.system("kaggle datasets download -d akshatgupta7/crop-yield-in-indian-states-dataset -p . --unzip")
+
 # Load the dataset
-data = pd.read_csv('/kaggle/input/crop-yield-in-indian-states-dataset/crop_yield.csv')
+data = pd.read_csv("crop_yield.csv")
 
 # Explore the data
 print(data.head())
 
 # Save the preprocessed data
-data.to_csv('BronzeLayer_data.csv', index=False)
+data.to_csv("BronzeLayer_data.csv", index=False)
+
